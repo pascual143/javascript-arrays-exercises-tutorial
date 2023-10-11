1268a8cdb22050aba00d2a77cf4092f6f6f468d1
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-11 17:18:09 +0000

    exercises

--- exercises/02.1-Access_and_retrieve/app.js
+++ exercises/02.1-Access_and_retrieve/app.js
@@ -2,7 +2,7 @@
 let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
 // 1. print the 3rd item here
-
+console.log(myArray[4]);
 // 2. change the 'thursday' value to null here
 
 // 3. print the position of step 2
