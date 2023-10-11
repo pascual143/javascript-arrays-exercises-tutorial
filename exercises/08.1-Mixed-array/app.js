e0d692d19b5d856b117eb4f3f5dbedf8deefffed
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-11 19:21:34 +0000

    exercises8.2

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,9 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 // Your code here
+let typeArray = []
+for (let i = 0; i < mix.length; i++){
+    typeArray.push( typeof mix[i] )
+}
+
+console.log(typeArray)
