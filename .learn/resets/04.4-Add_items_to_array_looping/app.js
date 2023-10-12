23fcea160cb333ff08bc5927e8331847a50b769b
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-12 18:29:47 +0000

    exercises10

---
+++ .learn/resets/04.4-Add_items_to_array_looping/app.js
@@ -0,0 +1,11 @@
+let arr = [4,5,734,43,45];
+
+//*****************
+// Your code here
+// you need to loop 10 times, for example, using a for loop
+//*****************
+for(let i = 0; i <= 9; i++) {
+    arr.push(Math.floor(Math.random() * 10))
+}
+
+console.log(arr)
--- .learn/resets/10-Everything-is-awesome/app.js
+++ .learn/resets/10-Everything-is-awesome/app.js
@@ -4,11 +4,6 @@ const ZerosToYahoos = (arr) => {
     let return_array = [];
     arr.forEach((item) => {
             // magic goes inside these brackets
-            if (item === 1) {
-                return_array.push(item)
-            } else {
-                return_array.push('Yahoo')
-            }
     });
     return return_array;
 };
